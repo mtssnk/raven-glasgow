3bc59a9c4ad42040c3d5bdbc6ee53e3a7fd9bfcb
mtssnk <mailmtsnk@gmail.com>
2026-05-20 14:42:15 +0100

    disable gutenberg, theme screenshot, tweaks

--- wp-content/themes/raven/functions/theme.php
+++ wp-content/themes/raven/functions/theme.php
@@ -98,3 +98,23 @@ add_filter( 'nav_menu_link_attributes', function ( $atts, $item, $args, $depth )
     }
     return $atts;
 }, 10, 4 );
+
+// Disable Gutenberg on the back end.
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg for widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
+
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the front end.
+    wp_dequeue_style( 'wp-block-library' );
+
+    // Remove Gutenberg theme.
+    wp_dequeue_style( 'wp-block-library-theme' );
+
+    // Remove inline global CSS on the front end.
+    wp_dequeue_style( 'global-styles' );
+
+    // Remove classic-themes CSS for backwards compatibility for button blocks.
+    wp_dequeue_style( 'classic-theme-styles' );
+}, 20 );
